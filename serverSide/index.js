b237cdf72907a7cccf45ee1a37094ad90daa4dfd
Dulon18 <dulon587@gmail.com>
2023-12-28 19:44:22 +0600

    update routes

--- serverSide/index.js
+++ serverSide/index.js
@@ -16,21 +16,39 @@ const wishController = require('../controllers/wishController');
 const orderController = require('../controllers/orderController');
 const invoiceController = require('../controllers/invoiceController');
 const reviewController = require('../controllers/reviewController');
+const productSliderController = require('../controllers/productSliderController');
 
 
 // Product Routes
-router.get('/ProductListByBrand/:BrandID', brandController.getBrands);
-router.get('/ProductListByCategory/:CategoryID', categoryController.getBrands);
-router.get('/ProductListBySmilier/:CategoryID', brandController.getBrands);
-router.get('/ProductListByKeyword/:Keyword', brandController.getBrands);
-router.get('/ProductListByRemark/:Remark', brandController.getBrands);
-router.get('/ProductDetails/:ProductID', productController.getBrands);
-router.get('/ProductReviewList/:ProductID', productController.getBrands);
-router.get('/ProductListBySmilier/:CategoryID', brandController.getBrands);
+router.get('/ProductBrandList',brandController.productBrandList)
+router.get('/ProductCategoryList',categoryController.categoryList)
+router.get('/ProductSliderList',productSliderController.productSlider)
+router.get('/ProductListByBrand/:BrandID', brandController.productListByBrand);
+router.get('/ProductListByCategory/:CategoryID', productController.productListByCategory);
+router.get('/ProductListByKeyword/:Keyword', brandController.productListByKeyword);
+router.get('/ProductListByRemark/:Remark', brandController.productListByRemark);
+router.get('/ProductDetails/:ProductID', productController.productDetails);
+router.get('/ProductReviewList/:ProductID', productController.productReview);
+router.get('/ProductListBySmilier/:CategoryID', brandController.similarProduct);
+
+//user
+router.get('/UserOTP/:email', userController.userOtp);
+router.get('/VerifyLogin/:email/:otp', userController.verifyLogin);
+router.get('/UserLogout', userController.logout);
+
+//profile
+router.post('/CreateProfile', profileController.profileCreate);
+router.post('/UpdateProfile', profileController.updateProfile);
+router.get('/ReadProfile', profileController.readProfile);
+
+router.post('/SaveWishList',wishController.saveWishList)
+router.post('/RemoveWishList',wishController.removeWishList)
+router.get('/WishList',wishController.wishList)
 
 //cartlist
-router.get('/CartList', cartController.getCarts);
-
+router.get('/CartList', cartController.cartsList);
+router.post('/SaveCartList', cartController.saveCart);
+router.post('/RemoveCartList', cartController.removeCart);
 module.exports = router;
 
 app.listen(3030, () =>
